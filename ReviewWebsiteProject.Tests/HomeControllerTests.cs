89db5761c2238f10b588f9356c032c3fa0c04672
Dan Rasmussen <drasmussen49@gmail.com>
2020-02-13 10:12:10 -0500

    Added test in HomeController tests, made it pass, added Index action to HomeController

--- ReviewWebsiteProject.Tests/HomeControllerTests.cs
+++ ReviewWebsiteProject.Tests/HomeControllerTests.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using ReviewWebsiteProject.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ReviewWebsiteProject.Tests
 {
     public class HomeControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Index_Returns_Hello_World()
         {
+            var underTest = new HomeController();
 
+            var result = underTest.Index();
+
+            Assert.Equal("Hello World", result);
         }
     }
 }
---
+++ ReviewWebsiteProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReviewWebsiteProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
