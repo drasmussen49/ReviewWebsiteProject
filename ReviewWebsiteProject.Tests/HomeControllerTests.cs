363152b58c6c93d42cbe2a8216d6e534d719fa72
Dan Rasmussen <drasmussen49@gmail.com>
2020-02-13 10:02:39 -0500

    Updated Startup.cs, added test project and linked it to main project

---
+++ ReviewWebsiteProject.Tests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ReviewWebsiteProject.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
