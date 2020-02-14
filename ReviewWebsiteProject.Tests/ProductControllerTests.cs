d789a634182931826fbb4b4a8397b998ca775a23
Dan Rasmussen <drasmussen49@gmail.com>
2020-02-14 14:01:30 -0500

    Added test class for ProductController, added ProductModel and associated properties/constructors

---
+++ ReviewWebsiteProject.Tests/ProductControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReviewWebsiteProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ReviewWebsiteProject.Tests
+{
+    public class ProductControllerTests
+    {
+        ProductController controller;
+        public ProductControllerTests()
+        {
+            controller = new ProductController();
+        }
+        
+        [Fact]
+        public void ProductIndex_Return_View()
+        {
+            var result = controller.ProductIndex();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ ReviewWebsiteProject/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewWebsiteProject.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ProductModel()
+        {
+
+        }
+
+        public ProductModel(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
