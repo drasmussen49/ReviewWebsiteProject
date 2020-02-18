e9b1469a3d90c4cc2a820e21c340909f3d9e3669
Eric Drexler <ericdrxlr@gmail.com>
2020-02-18 14:31:05 -0500

    added database and properties and override

---
+++ ReviewWebsiteProject/ArcadeContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewWebsiteProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ReviewWebsiteProject
+{
+    public class ArcadeContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=ArcadeDb;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
