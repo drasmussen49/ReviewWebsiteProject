f5b1390598ee76caa87a0b029087c6a7a6ce8da4
Eric Drexler <ericdrxlr@gmail.com>
2020-02-20 09:57:37 -0500

    deleted dic. and uopdated arcade context

--- ReviewWebsiteProject/Repositories/ProductRepository.cs
+++ ReviewWebsiteProject/Repositories/ProductRepository.cs
@@ -8,17 +8,11 @@ namespace ReviewWebsiteProject.Repositories
 {
     public class ProductRepository : IRepository<Product>
     {
-        public Dictionary<int, Product> productDictionary;
         private ArcadeContext db;
 
-        public ProductRepository()
+        public ProductRepository(ArcadeContext db)
         {
-            productDictionary = new Dictionary<int, Product>()
-            {
-                {1, new Product(1, "Pac-Man", "Yellow, fruit-eating monster", "I bought this arcade game, now I hate ghosts", "/img/pacman.jpg")},
-                {2, new Product(2, "Gauntlet Legends", "Save the Realms from certain doom", "Wtf I love this game", "/img/GauntletLegends.jpg")},
-                {3, new Product(3, "DigDug", "Dig and dug", "Being inflated until you explode has got to hurt!", "/img/digdug.jpg")}
-            };
+            this.db = db;
         }
 
         public object Count()
@@ -26,19 +20,14 @@ namespace ReviewWebsiteProject.Repositories
             return db.Products.Count();
         }
 
-        public ProductRepository(ArcadeContext db)
-        {
-            this.db = db;
-        }
-
         public IEnumerable<Product> GetAll()
         {
-            return productDictionary.Values;
+            return db.Products;
         }
 
         public Product GetById(int id)
         {
-            return productDictionary[id];
+            return db.Products.Single(p => p.Id == id);
         }
     }
 }
