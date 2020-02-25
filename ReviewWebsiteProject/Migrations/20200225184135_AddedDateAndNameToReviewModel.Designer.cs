8143d2f5dfdbe99b5d2caeba9a36c3a46e413bb1
Eric Drexler <ericdrxlr@gmail.com>
2020-02-25 13:59:31 -0500

    colored details word and added hr

--- ReviewWebsiteProject/Migrations/20200225184135_AddedDateAndNameToReviewModel.Designer.cs
+++ ReviewWebsiteProject/Migrations/20200225184135_AddedDateAndNameToReviewModel.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReviewWebsiteProject;
 
 namespace ReviewWebsiteProject.Migrations
 {
     [DbContext(typeof(ArcadeContext))]
-    partial class ArcadeContextModelSnapshot : ModelSnapshot
+    [Migration("20200225184135_AddedDateAndNameToReviewModel")]
+    partial class AddedDateAndNameToReviewModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
