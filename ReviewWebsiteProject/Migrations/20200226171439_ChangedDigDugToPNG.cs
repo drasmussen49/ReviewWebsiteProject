a80469ff8d7e41052756a7f8a42457f20fd9dd19
TShaw516 <TShaw516@gmail.com>
2020-02-26 14:20:22 -0500

    added hr style for dividing content

---
+++ ReviewWebsiteProject/Migrations/20200226171439_ChangedDigDugToPNG.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewWebsiteProject.Migrations
+{
+    public partial class ChangedDigDugToPNG : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "/img/digdug.png");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "/img/digdug.jpg");
+        }
+    }
+}
