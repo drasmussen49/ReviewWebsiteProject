e24f3bf7d0b0fb822e83c4a5278984d4cda5a31f
Eric Drexler <ericdrxlr@gmail.com>
2020-02-26 15:17:14 -0500

    small adjustments home page and index titles

--- ReviewWebsiteProject/Migrations/20200226183129_AddedMoreProducts.cs
+++ ReviewWebsiteProject/Migrations/20200226183129_AddedMoreProducts.cs
@@ -6,9 +6,7 @@ namespace ReviewWebsiteProject.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Date",
-                table: "Reviews");
+          
 
             migrationBuilder.UpdateData(
                 table: "Products",
